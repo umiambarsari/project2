05f7013d545c7dc82e0da73e3893e11fe883be2a
umiambarsari <uambarsari89@gmail.com>
2020-10-30 22:44:30 +0700

    Initial commit

---
+++ system/resources/views/template/login.blade.php
@@ -0,0 +1,287 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>login</title>
+	<link type="text/css" rel="stylesheet" href="{{url('public')}}/css/style.css"/>
+
+	<style type="text/css">
+
+HOME
+SHOP
+TUTORIAL
+CIRCLE
+DEMO
+BUKTI PENGIRIMAN
+LABORATORIUM
+Home » Membuat Desain Form Login Dengan CSS
+
+Web Design
+Membuat Desain Form Login Dengan CSS
+
+By Diki Alfarabi Hadi
+24 September 2018
+Web Design
+
+ 
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Membuat Desain Form Login Dengan CSS - www.malasngoding.com</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+	<h1>Membuat Desain Form Login Dengan CSS <br/> www.malasngoding.com</h1>
+
+	<div class="kotak_login">
+		<p class="tulisan_login">Silahkan login</p>
+
+		<form>
+			<label>Username</label>
+			<input type="text" name="username" class="form_login" placeholder="Username atau email ..">
+
+			<label>Password</label>
+			<input type="text" name="password" class="form_login" placeholder="Password ..">
+
+			<input type="submit" class="tombol_login" value="LOGIN">
+
+			<br/>
+			<br/>
+			<center>
+				<a class="link" href="https://www.malasngoding.com">kembali</a>
+			</center>
+		</form>
+		
+	</div>
+
+
+</body>
+</html>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Membuat Desain Form Login Dengan CSS - www.malasngoding.com</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+ 
+	<h1>Membuat Desain Form Login Dengan CSS <br/> www.malasngoding.com</h1>
+ 
+	<div class="kotak_login">
+		<p class="tulisan_login">Silahkan login</p>
+ 
+		<form>
+			<label>Username</label>
+			<input type="text" name="username" class="form_login" placeholder="Username atau email ..">
+ 
+			<label>Password</label>
+			<input type="text" name="password" class="form_login" placeholder="Password ..">
+ 
+			<input type="submit" class="tombol_login" value="LOGIN">
+ 
+			<br/>
+			<br/>
+			<center>
+				<a class="link" href="https://www.malasngoding.com">kembali</a>
+			</center>
+		</form>
+		
+	</div>
+ 
+ 
+</body>
+</html>
+
+
+
+
+<div class="kotak_login">
+	<p class="tulisan_login">Silahkan login</p>
+
+	<form>
+		<label>Username</label>
+		<input type="text" name="username" class="form_login" placeholder="Username atau email ..">
+
+		<label>Password</label>
+		<input type="text" name="password" class="form_login" placeholder="Password ..">
+
+		<input type="submit" class="tombol_login" value="LOGIN">
+
+		<br/>
+		<br/>
+		<center>
+			<a class="link" href="https://www.malasngoding.com">kembali</a>
+		</center>
+	</form>
+	
+</div>
+<div class="kotak_login">
+	<p class="tulisan_login">Silahkan login</p>
+ 
+	<form>
+		<label>Username</label>
+		<input type="text" name="username" class="form_login" placeholder="Username atau email ..">
+ 
+		<label>Password</label>
+		<input type="text" name="password" class="form_login" placeholder="Password ..">
+ 
+		<input type="submit" class="tombol_login" value="LOGIN">
+ 
+		<br/>
+		<br/>
+		<center>
+			<a class="link" href="https://www.malasngoding.com">kembali</a>
+		</center>
+	</form>
+	
+</div>
+
+
+<link rel="stylesheet" type="text/css" href="style.css">
+<link rel="stylesheet" type="text/css" href="style.css">
+
+
+body{
+	font-family: sans-serif;
+	background: #d5f0f3;
+}
+
+h1{
+	text-align: center;
+	/*ketebalan font*/
+	font-weight: 300;
+}
+
+.tulisan_login{
+	text-align: center;
+	/*membuat semua huruf menjadi kapital*/
+	text-transform: uppercase;
+}
+
+.kotak_login{
+	width: 350px;
+	background: white;
+	/*meletakkan form ke tengah*/
+	margin: 80px auto;
+	padding: 30px 20px;
+}
+
+label{
+	font-size: 11pt;
+}
+
+.form_login{
+	/*membuat lebar form penuh*/
+	box-sizing : border-box;
+	width: 100%;
+	padding: 10px;
+	font-size: 11pt;
+	margin-bottom: 20px;
+}
+
+.tombol_login{
+	background: #46de4b;
+	color: white;
+	font-size: 11pt;
+	width: 100%;
+	border: none;
+	border-radius: 3px;
+	padding: 10px 20px;
+}
+
+.link{
+	color: #232323;
+	text-decoration: none;
+	font-size: 10pt;
+}
+body{
+	font-family: sans-serif;
+	background: #d5f0f3;
+}
+ 
+h1{
+	text-align: center;
+	/*ketebalan font*/
+	font-weight: 300;
+}
+ 
+.tulisan_login{
+	text-align: center;
+	/*membuat semua huruf menjadi kapital*/
+	text-transform: uppercase;
+}
+ 
+.kotak_login{
+	width: 350px;
+	background: white;
+	/*meletakkan form ke tengah*/
+	margin: 80px auto;
+	padding: 30px 20px;
+}
+ 
+label{
+	font-size: 11pt;
+}
+ 
+.form_login{
+	/*membuat lebar form penuh*/
+	box-sizing : border-box;
+	width: 100%;
+	padding: 10px;
+	font-size: 11pt;
+	margin-bottom: 20px;
+}
+ 
+.tombol_login{
+	background: #46de4b;
+	color: white;
+	font-size: 11pt;
+	width: 100%;
+	border: none;
+	border-radius: 3px;
+	padding: 10px 20px;
+}
+ 
+.link{
+	color: #232323;
+	text-decoration: none;
+	font-size: 10pt;
+}
+
+
+	</style>
+</head>
+<body>
+
+	
+	<div class="kotak_login">
+		<p class="tulisan_login">Silahkan login</p>
+
+		<form>
+			<label>Username</label>
+			<input type="text" name="username" class="form_login" placeholder="Username atau email ..">
+
+			<label>Password</label>
+			<input type="text" name="password" class="form_login" placeholder="Password ..">
+
+			<input type="submit" class="tombol_login" value="LOGIN">
+
+			<br/>
+			<br/>
+			<center>
+				<a class="link" href="https://www.malasngoding.com">kembali</a>
+			</center>
+		</form>
+		
+	</div>
+
+
+</body>
+</html>
---
+++ system/routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/template', function () {
+    return view('template.base');
+});
+
+Route::get('/detail', function () {
+    return view('template.detail');
+});
+
+Route::get('/login', function () {
+    return view('template.login');
+});
